be25e45ba13b84fa29b6ef4b77000e09106ad641
tx7do <yanglinbo@gmail.com>
2022-12-20 20:32:45 +0800

    feat: refactor.

---
+++ pkg/util/bootstrap/wire.go
@@ -0,0 +1,9 @@
+package bootstrap
+
+// FIX: missing go.sum entry for module providing package XXXXXXXXXXXXXXXXXXXXX
+
+import (
+	_ "github.com/google/subcommands"
+	_ "golang.org/x/tools/go/ast/astutil"
+	_ "golang.org/x/tools/go/packages"
+)
